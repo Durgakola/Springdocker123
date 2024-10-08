52bafbe82eba57c4c8f3652c5168d99d3ac6c078
Durgakola <Durgakola50@gmail.com>
2024-10-08 12:38:07 +0530

    spring1

--- src/main/java/com/neoteric/Dokcer/Controller.java
+++ src/main/java/com/neoteric/Dokcer/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @GetMapping("/jenkins")
-    public  void Jenkins(){
-        System.out.println("jenkins");
+    public String Jenkins(){
+        return "jenkins";
     }
 }
