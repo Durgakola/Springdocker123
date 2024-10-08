506c4f76b580feb3673d528b27988f0bcfb9a29f
Durgakola <Durgakola50@gmail.com>
2024-10-08 14:36:50 +0530

    spring1

--- src/main/java/com/neoteric/Dokcer/Controller.java
+++ src/main/java/com/neoteric/Dokcer/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/jenkins")
     public String Jenkins(){
-        return "jenkins";
+        return "jenkins from docker";
     }
 }
